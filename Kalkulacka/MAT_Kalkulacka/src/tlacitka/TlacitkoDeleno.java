7a5f488cd94cfab0e10ea3d45ff37f9423d6992d
Trollface7272 <trollface7272@post.cz>
2021-09-20 12:13:05 +0200

    a

--- Kalkulacka/MAT_Kalkulacka/src/tlacitka/TlacitkoDeleno.java
+++ Kalkulacka/MAT_Kalkulacka/src/tlacitka/TlacitkoDeleno.java
@@ -12,7 +12,6 @@ public class TlacitkoDeleno implements I_Algoritmus {
         String radek1 = mujModel.getRadek1();
         String radek2 = mujModel.getRadek2();
         if (radek1.equals("")) return;
-        if (Integer.parseInt(radek1) == 0) return;
         mujModel.setRadek2(radek2 + radek1 + "/");
         mujModel.setRadek1("");
     }
--- Kalkulacka/MAT_Kalkulacka/src/tlacitka/TlacitkoRovnase.java
+++ Kalkulacka/MAT_Kalkulacka/src/tlacitka/TlacitkoRovnase.java
@@ -4,6 +4,8 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import kalkulacka1.Model;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class TlacitkoRovnase implements I_Algoritmus {
     private final Model mujModel;
@@ -12,16 +14,32 @@ public class TlacitkoRovnase implements I_Algoritmus {
     }
     @Override
     public void Algoritmus() {
+        zpusob2();
+    }
+    private void zpusob1() {
+        if (mujModel.getRadek1().equals("")) return;
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine js = manager.getEngineByName("js");
+        mujModel.setRadek2(mujModel.getRadek2() + mujModel.getRadek1());
         try {
             mujModel.setRadek2(
-                    js.eval(mujModel.getRadek1()).toString()
+                    js.eval(
+                            mujModel.getRadek2()
+                    ).toString()
             );
             mujModel.setRadek1("");
+            
         } catch (ScriptException ex) {
-            System.out.println("Error");
+            System.out.println("Error\n" + ex.getMessage());
         }
+    }
+    private void zpusob2() {
+        if (mujModel.getRadek1().equals("")) return;
+        mujModel.setRadek2(mujModel.getRadek2() + mujModel.getRadek1());
+
+        Expression exp = new ExpressionBuilder(mujModel.getRadek2()).build();
+        mujModel.setRadek2(Double.toString(exp.evaluate()));
+        mujModel.setRadek1("");
     }
 
 }
