ccaf2495cd16da6309f135f765ab965976294081
Trollface7272 <trollface7272@post.cz>
2021-10-04 10:48:04 +0200

    a

--- Sibenice/MAT_Sibenice/src/mat_sibenice/DoplnovaniSlova.java
+++ Sibenice/MAT_Sibenice/src/mat_sibenice/DoplnovaniSlova.java
@@ -12,6 +12,8 @@ public class DoplnovaniSlova extends JPanel {
         delkaSlova = 0;
         slovo = "";
         pismenaSlova = new char[0];
+        noveSlovo("ahoj");
+        zkusPismeno('a');
     }
     
     //Metody rozhraní
@@ -19,10 +21,16 @@ public class DoplnovaniSlova extends JPanel {
         this.delkaSlova = slovo.length();
         this.slovo = slovo;
         this.pismenaSlova = slovo.replaceAll("[^.!?\\s]", "_").toCharArray();
-        System.out.println(slovo.replaceAll("[^.!?\\s]", "_"));
+        
         this.repaint();
     }
     public boolean zkusPismeno(char pismeno) {
+        if (!this.slovo.contains(String.valueOf(pismeno))) return false;
+        for (int i = 0; i < this.slovo.length(); i++) {
+            if (this.slovo.charAt(i) != pismeno) continue;
+            this.pismenaSlova[i] = pismeno;
+            System.out.println(this.pismenaSlova.toString());
+        }
         
         return false;
     }
--- Sibenice/MAT_Sibenice/src/mat_sibenice/MAT_Sibenice.java
+++ Sibenice/MAT_Sibenice/src/mat_sibenice/MAT_Sibenice.java
@@ -6,5 +6,6 @@ public class MAT_Sibenice {
     public static void main(String[] args) {
         View view = new View();
         view.setVisible(true);
+        new DoplnovaniSlova();
     }
 }
